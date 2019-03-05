a2d5289e23a464bef51dd6c79f00c11ca94bc267
lin <469946668@qq.com>
2019-03-05 20:26:04 +0800

    -

--- src/bootstrap-toasts.js
+++ src/bootstrap-toasts.js
@@ -136,10 +136,11 @@ function toasts_style() {
     style.id = "bootstrap_toasts_css";
     style.innerHTML = "" +
         ".toasts-icons {\n" +
-        // "    position: absolute;\n" +
         "    width: 22px;\n" +
         "    height: 22px;\n" +
-        "    left: 10px;\n" +
+        "    display: inline-block;\n" +
+        "    margin-left: 5px;\n" +
+        "    margin-top: 5px;\n" +
         "}\n" +
         ".toasts-icon-success {\n" +
         "    background-image: url('data:image/svg+xml,%3Csvg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 52 52\"%3E%3Cg fill=\"%234c4c4c\"%3E%3Cpath d=\"M26 0C11.664 0 0 11.663 0 26s11.664 26 26 26 26-11.663 26-26S40.336 0 26 0zm0 50C12.767 50 2 39.233 2 26S12.767 2 26 2s24 10.767 24 24-10.767 24-24 24z\"/%3E%3Cpath d=\"M38.252 15.336l-15.369 17.29-9.259-7.407a1 1 0 0 0-1.249 1.562l10 8a.999.999 0 0 0 1.373-.117l16-18a1 1 0 1 0-1.496-1.328z\"/%3E%3C/g%3E%3C/svg%3E');\n" +
